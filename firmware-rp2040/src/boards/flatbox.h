639bccc5ecd30c66fc4055c761c88dd46ae0f5ce
Jacek Fedorynski <jfedor@jfedor.org>
2022-11-21 19:41:52 +0100

    Better compatibility with flash chips
    When building firmware for rev4 and rev5, use slower settings for
    flash that should give better compatibility with different chips or
    even samples of the same chip.
    
     #define PICO_XOSC_STARTUP_DELAY_MULTIPLIER 64
     #define PICO_BOOT_STAGE2_CHOOSE_GENERIC_03H 1
     #define PICO_FLASH_SPI_CLKDIV 4

---
+++ firmware-rp2040/src/boards/flatbox.h
@@ -0,0 +1,27 @@
+#ifndef _BOARDS_FLATBOX_H
+#define _BOARDS_FLATBOX_H
+
+// On some samples, the xosc can take longer to stabilize than is usual
+#ifndef PICO_XOSC_STARTUP_DELAY_MULTIPLIER
+#define PICO_XOSC_STARTUP_DELAY_MULTIPLIER 64
+#endif
+
+// Use slower generic flash access
+#define PICO_BOOT_STAGE2_CHOOSE_GENERIC_03H 1
+
+#ifndef PICO_FLASH_SPI_CLKDIV
+#define PICO_FLASH_SPI_CLKDIV 4
+#endif
+
+// rev4 actually has 16MB of flash, but whatever
+
+#ifndef PICO_FLASH_SIZE_BYTES
+#define PICO_FLASH_SIZE_BYTES (2 * 1024 * 1024)
+#endif
+
+// All boards have B1 RP2040
+#ifndef PICO_RP2040_B0_SUPPORTED
+#define PICO_RP2040_B0_SUPPORTED 0
+#endif
+
+#endif
